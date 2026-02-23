954055db05eca9eb71e99259affea071d2e9cd24
Mark Eschweiler <mark.eschweiler@uni-koeln.de>
2026-02-23 11:15:44 +0100

    Bundling dependancies

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,6 @@ export default defineConfig({
             name: 'MimirExplorer',
             formats: ['es', 'umd'],
             fileName: (format) => (format === 'es' ? 'mimir.es.js' : 'mimir.umd.cjs')
-        },
-        rollupOptions: {
-            external: ['openseadragon', '@google/model-viewer']
         }
     }
 });
